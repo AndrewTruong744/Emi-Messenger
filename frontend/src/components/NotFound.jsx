e74bf07791bd8926b87aaeed1e980a7005c45aed
AndrewTruong744 <andrewtruong40024@gmail.com>
2025-12-24 15:09:08 -0600

    implemented NotFound component

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,8 +1,15 @@
 import {useState, useEffect} from 'react';
 import styles from "../styles/NotFound.module.css";
+import EmiSleepy from "../assets/EmiSleepy.jpg";
 
 function NotFound() {
-  
+
+  return (
+    <div className={styles.notFound}>
+      <img src={EmiSleepy} alt="Emi CEO Sleepy" className={styles.image}/>
+      <h1>This page sadly does not exist</h1>
+    </div>
+  );
 }
 
 export default NotFound;
